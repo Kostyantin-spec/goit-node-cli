cdcf2ea9c3606a4176fc3bea9c7c3ba180c6df91
Kostyantin-spec <134406433+Kostyantin-spec@users.noreply.github.com>
2024-03-09 18:44:46 +0200

    start

---
+++ index.js
@@ -0,0 +1,41 @@
+import { program } from "commander";
+import * as contactsService from "./contacts.js";
+
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
+
+program.parse();
+
+const options = program.opts();
+
+// TODO: рефакторити
+async function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+      const allContacts = await contactsService.listContacts();
+      return console.table(allContacts);
+
+    case "get":
+      const contact = await contactsService.getContactById(id);
+      return console.log(contact);
+
+    case "add":
+      const addedContact = await contactsService.addContact(name, email, phone);
+      return console.log(addedContact);
+      break;
+
+    case "remove":
+      const removedContact = await contactsService.removeContact(id);
+      return console.log(removedContact);
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
+
+invokeAction(options);
